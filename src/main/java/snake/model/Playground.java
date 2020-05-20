6041d95e9cfb692b0913a82221f15f17ecae807d
Butterpod <mithian1234@gmail.com>
2020-05-20 16:05:25 +0200

    add classes of snake game

--- src/main/java/snake/model/Playground.java
+++ src/main/java/snake/model/Playground.java
@@ -1,4 +1,17 @@
 package snake.model;
 
 public class Playground {
+
+    private int width;
+    private  int height;
+    private char[][] snakeBoard;
+
+    public Playground(int width, int height){
+        this.width=width;
+        this.height=height;
+        this.snakeBoard= new char[this.height][this.width];
+
+    }
+
+
 }
--- src/main/java/snake/model/Snake.java
+++ src/main/java/snake/model/Snake.java
@@ -1,5 +1,47 @@
 package snake.model;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class Snake {
 
+    private int applesEaten;
+    private Position posSnakehead;
+    private Deque tail;
+
+    public Snake(int applesEaten, Position posSnakehead, Deque tail) {
+        this.applesEaten = applesEaten;
+        this.posSnakehead = posSnakehead;
+        this.tail = tail;
+    }
+
+    public Snake(){
+        applesEaten =0;
+        posSnakehead = new Position(0,0);
+        tail=new LinkedList();
+    }
+
+    public int getApplesEaten() {
+        return applesEaten;
+    }
+
+    public void setApplesEaten(int applesEaten) {
+        this.applesEaten = applesEaten;
+    }
+
+    public Position getPosSnakehead() {
+        return posSnakehead;
+    }
+
+    public void setPosSnakehead(Position posSnakehead) {
+        this.posSnakehead = posSnakehead;
+    }
+
+    public Deque getTail() {
+        return tail;
+    }
+
+    public void setTail(Deque tail) {
+        this.tail = tail;
+    }
 }
